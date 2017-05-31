516b578743cb6b4e892b41b3c2f8b6f80d5dacb9
Nhung Hoang <nhoang4673@gmail.com>
2017-05-31 15:14:43 -0400

    progress on stereo

--- stereo_detect.py
+++ stereo_detect.py
@@ -10,6 +10,8 @@ from random import shuffle
 from scipy import io, signal
 from sys import argv
 
+######################################################
+
 def downsize(img):
 	# downsize depth map 
 	# 480x640 to 12x16 (downsized by factor of 40)
@@ -25,6 +27,8 @@ def downsize(img):
 			if non_zero.size != 0:
 				downsized_img[i/40,j/40] = np.mean(non_zero)  
 	return downsized_img
+
+######################################################
 
 def get_data():
 	print "loading 'forensics' data..."
@@ -75,6 +79,8 @@ def get_data():
 	np.savez('all', audio=audio, depth=new_depth)
 	print "concatenated audio and downsized depth data saved as 'all.npz'\n"
 
+######################################################
+
 def preprocess_data():
 	data = np.load('/home/hoangnt/echolocation/all.npz')
 	audio = data['audio']
@@ -85,14 +91,14 @@ def preprocess_data():
 	valid_depth = depth_reshaped[valid_bool] # removes samples containing zero
 	# valid_depth shape: 10291, 192 (22% loss)
 #	print "\n\nVALID DEPTH SHAPE:",valid_depth.shape,"\n\n"
-
+	
 	new_audio = audio[valid_bool]
 	freq1, time1, spectro1 = signal.spectrogram(new_audio[0,:,0], noverlap=250)
 	freq2, time2, spectro2 = signal.spectrogram(new_audio[0,:,1], noverlap=250)
 	input_set = np.empty((new_audio.shape[0], spectro1.shape[0], spectro1.shape[1], 2))
 	input_set[0,:,:,0] = spectro1
 	input_set[0,:,:,1] = spectro2
-
+	'''
 	for i in range(1,new_audio.shape[0]):
 		freq1, time1, spectro1 = signal.spectrogram(new_audio[i,:,0], noverlap=250)
 		freq2, time2, spectro2 = signal.spectrogram(new_audio[i,:,1], noverlap=250)
@@ -100,13 +106,125 @@ def preprocess_data():
 		input_set[i,:,:,1] = spectro2
 #		plt.pcolormesh(spectro2)
 #	plt.show()
-
+	'''
 	return input_set, valid_depth
+
+######################################################
+
+def split_data(x, y):
+
+	'''
+	xshape = x.shape[0]
+	xbool = np.zeros_like(x, dtype=bool)
+	ybool = np.zeros_like(y, dtype=bool)
+	A = int((0.8/14)*xshape)
+	B = int((1.6/14)*xshape)
+	C = int((12.8/14)*xshape)
+	sss = subset_size = int(xshape/7)
+	tss = test_set_size = int((0.2*xshape)/7)
+
+	xbool[A:tss] = True 
+	xbool[sss+A:sss+A+tss] = True
+	xbool[2*sss+A:2*sss+A+tss] =True 
+	xbool[3*sss+A:3*sss+A+tss] =True 
+	xbool[4*sss+A:4*sss+A+tss] =True 
+	xbool[5*sss+A:5*sss+A+tss] =True 
+	xbool[6*sss+A:6*sss+A+tss] =True 
+
+	ybool[A:tss] = True 
+	ybool[sss+A:sss+A+tss] = True
+	ybool[2*sss+A:2*sss+A+tss] =True 
+	ybool[3*sss+A:3*sss+A+tss] =True 
+	ybool[4*sss+A:4*sss+A+tss] =True 
+	ybool[5*sss+A:5*sss+A+tss] =True 
+	ybool[6*sss+A:6*sss+A+tss] =True 
+
+	xtest = x[xbool]
+	ytest = y[ybool]
+	xtrain = x[np.logical_not(xbool)]
+	ytrain = y[np.logical_not(ybool)]
+	'''
+  
+	'''# Shuffle the data 
+	combined = zip(input_set, target)
+	shuffle(combined)
+	input_set, target = zip(*combined)
+	input_set = np.asarray(input_set)
+	target = np.asarray(target)
+
+	length = int(input_set.shape[0]*0.8)
+	xtrain = input_set[:length]
+	ytrain = target[:length]
+	xtest = input_set[length:]
+	ytest = target[length:]'''
+
+	
+	xshape = x.shape[0]
+	xbool = np.zeros_like(x, dtype=bool)
+	ybool = np.zeros_like(y, dtype=bool)
+	subset_size = int(xshape/7)
+	half_test_size = int((xshape*0.2)/14)
+	
+	for i in range(7):
+		start_index = subset_size*i
+		end_index = start_index + subset_size
+		mid_index = int((0.5*(end_index-start_index)) + start_index) 
+		test_start = mid_index-half_test_size
+		test_end = mid_index+half_test_size 
+		xbool[test_start:test_end] = True
+		ybool[test_start:test_end] = True 
+
+	# ISSUE: these are all 1D arrays, can't reshape because unknown first dim
+	# hardcoded reshapes for quick fix now 
+	xtest = x[xbool].reshape((2058,129,385,2))
+	ytest = y[ybool].reshape((2058,192))
+	xtrain = x[np.logical_not(xbool)].reshape((8233,129,385,2))
+	ytrain = y[np.logical_not(ybool)].reshape((8233,192))
+
+	print "x_all", input_set.shape 
+	print "y_all", target.shape
+	print "x_train", x_train.shape
+	print "y_train", y_train.shape
+	print "x_test", x_test.shape
+	print "y_test", y_test.shape
+
+	return load_model("depth_model.h5")
+
+######################################################
+
+def build_model(x_train, y_train):
+	net = Sequential()
+	INPUT_SHAPE = x_train.shape[1:]
+	net.add(Conv2D(64, (5, 5), strides=(1,1), activation='relu', input_shape=INPUT_SHAPE))
+	net.add(Flatten())
+	net.add(Dense(450, activation='relu'))
+	net.add(Dense(1, activation='linear'))
+	net.compile(optimizer='adam', loss='mean_squared_error')
+	net.fit(x_train, y_train, validation_split=0.2, epochs=50)
+	net.save("stereo_model.h5")
+	return load_model("stereo_model.h5")
+
+######################################################
+
+def run_model(net, x_test, y_test):
+	loss = net.evaluate(x_test, y_test)
+#	scale_loss = np.exp(loss)
+	predictions = net.predict(x_test)
+#	plot_data(np.exp(y_test), np.exp(predictions))
+	return loss #scale_loss
+
+######################################################
 
 def main():
 	while not os.path.isfile('all.npz'):
 		get_data()
 	input_set, target = preprocess_data()
+	x_train, y_train, x_test, y_test = split_data(input_set, target)
+	if not os.path.isfile("stereo_model.h5"):
+      model = build_model(x_train, y_train)
+    else: 
+      model = load_model("stereo_model.h5")
+#	loss = run_model(model, x_test, y_test)	
 
 main()
 
