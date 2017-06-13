146d72ba0f3751469c19557dfac9bd4eaa05b9a4
nhunghoang <nhoang4673@gmail.com>
2017-06-13 11:20:52 -0400

    latest versions of param tuning

--- stereo/network_raw.py
+++ stereo/network_raw.py
@@ -9,13 +9,20 @@ from keras.backend import floatx
 from keras.layers import Conv1D, Conv2D, Dense
 from keras.layers.core import Flatten
 from keras.models import load_model, Sequential
+from keras import optimizers
 from scipy import io, signal
+from sys import argv
 
 ######################################################
 
 def build_and_train_model(x_train, y_train):
+	adam = optimizers.Adam(lr=0.001)
+	nadam = optimizers.Nadam(lr=0.002)
+	sgd = optimizers.SGD(lr=0.01)
+	RMSprop = optimizers.RMSprop(lr=0.0001)
+
 	net = Sequential()
-	net.add(Conv2D(32, (128,2), 
+	net.add(Conv2D(64, (128,2), 
 					activation='relu', 
 					data_format='channels_last', 
 					input_shape=x_train.shape[1:]))
@@ -23,12 +30,14 @@ def build_and_train_model(x_train, y_train):
 	net.add(Flatten())
 	net.add(Dense(200, activation='relu'))
 	net.add(Dense(192, activation='linear'))
+#	net.add(Dense(48, activation='linear'))
 	net.compile(optimizer='adam', loss=adjusted_mse)
 	print "finished compiling"
-	net.fit(x_train, y_train, validation_split=0.2, epochs=50, batch_size=32)
-	net.save('stereo_model_rawA.h5')
-	print "model saved as 'stereo_model_rawA.h5'"
-	return load_model('stereo_model_rawA.h5', custom_objects={'adjusted_mse':adjusted_mse})
+	net.fit(x_train, y_train, validation_split=0.2, epochs=100, batch_size=32)
+#	net.save('stereo_model_rawA.h5')
+#	print "model saved as 'stereo_model_rawA.h5'"
+#	return load_model('stereo_model_rawA.h5', custom_objects={'adjusted_mse':adjusted_mse})
+	return net
 
 ######################################################
 
@@ -37,8 +46,8 @@ def run_model(net, x_test, y_test):
 	loss = net.evaluate(x_test, y_test)
 	print "\nLOSS:", loss
 	predictions = net.predict(x_test)
-	for i in range(100,2000, 110):
-		view_depth_maps(i, net, np.exp(y_test)-1,np.exp(predictions)-1)
+#	for i in range(100,2000, 110):
+	view_depth_maps(100, net, np.exp(y_test)-1,np.exp(predictions)-1)
 
 #####################################################
 
@@ -55,21 +64,43 @@ def adjusted_mse(y_true, y_pred):
 #####################################################
 
 def view_depth_maps(index, model, ytrue, ypred):
-	true = np.reshape(ytrue[index], (12,16))
-	pred = np.reshape(ypred[index], (12,16))
-	diff = np.absolute(pred - true)
+	'''true1 = np.reshape(ytrue[index], (6,8))
+	pred1 = np.reshape(ypred[index], (6,8))
+	true2 = np.reshape(ytrue[index+35], (6,8))
+	pred2 = np.reshape(ypred[index+35], (6,8))
+	true3 = np.reshape(ytrue[index+70], (6,8))
+	pred3 = np.reshape(ypred[index+70], (6,8))'''
 
-	ax1 = plt.subplot(1,3,1)
-	true_map = plt.imshow(true, interpolation='none')
-	ax1.set_title("True Depth")
+	true1 = np.reshape(ytrue[index], (12,16))
+	pred1 = np.reshape(ypred[index], (12,16))
+	true2 = np.reshape(ytrue[index+35], (12,16))
+	pred2 = np.reshape(ypred[index+35], (12,16))
+	true3 = np.reshape(ytrue[index+70], (12,16))
+	pred3 = np.reshape(ypred[index+70], (12,16))
+
+	ax1 = plt.subplot(3,2,1)
+	true_map1 = plt.imshow(true1, interpolation='none')
+	ax1.set_title("True Depth 1")
 	
-	ax2 = plt.subplot(1,3,2)
-	pred_map = plt.imshow(pred, interpolation='none')
-	ax2.set_title("Predicted Depth")
+	ax2 = plt.subplot(3,2,2)
+	pred_map1 = plt.imshow(pred1, interpolation='none')
+	ax2.set_title("Predicted Depth 1")
 	
-	ax3 = plt.subplot(1,3,3)
-	diff_map = plt.imshow(diff, interpolation='none', cmap='gray')
-	ax3.set_title("Difference") # goal is black
+	ax3 = plt.subplot(3,2,3)
+	true_map2 = plt.imshow(true2, interpolation='none')
+	ax3.set_title("True Depth 2")
+	
+	ax4 = plt.subplot(3,2,4)
+	pred_map2 = plt.imshow(pred2, interpolation='none')
+	ax4.set_title("Predicted Depth 2")
+
+	ax5 = plt.subplot(3,2,5)
+	true_map3 = plt.imshow(true3, interpolation='none')
+	ax5.set_title("True Depth 3")
+	
+	ax6 = plt.subplot(3,2,6)
+	pred_map3 = plt.imshow(pred3, interpolation='none')
+	ax6.set_title("Predicted Depth 3")
 	
 	plt.show()
 
@@ -77,10 +108,13 @@ def view_depth_maps(index, model, ytrue, ypred):
 
 def main():
 	#if not os.path.isfile('stereo_model_rawA.h5'):
-	if os.path.isfile('stereo_model_rawA.h5'):
+	if argv[1] == "1":
 		print "building model..."
 		path = os.getcwd()
-		with h5py.File(path+'/model_sets_rawA.h5', 'r') as sets:
+#		with h5py.File(path+'/model_sets_rawA.h5', 'r') as sets:
+#		with h5py.File(path+'/model_sets_rawA2.h5', 'r') as sets:
+		with h5py.File(path+'/model_sets_rawA3.h5', 'r') as sets:
+#		with h5py.File(path+'/model_sets_rawA_6x8.h5', 'r') as sets:
 			x_train = sets['xtrain'][:]
 			y_train = np.log(1+sets['ytrain'][:])
 			x_test = sets['xtest'][:]
--- stereo/stereo_processing.py
+++ stereo/stereo_processing.py
@@ -3,6 +3,7 @@ import numpy as np
 import os 
 import os.path
 
+from random import shuffle
 from scipy import signal
 from sys import argv
 
@@ -12,29 +13,32 @@ def downsize(img):
 	# downsize depth map 
 	# 480x640 to 12x16 (downsized by factor of 40)
 	# window size: 40x40 
-
+	
+	factor = 40
 	orig_dims = img.shape
-	ds_dims = (orig_dims[0]/40, orig_dims[1]/40)
+	ds_dims = (orig_dims[0]/factor, orig_dims[1]/factor)
 	downsized_img = np.zeros(ds_dims)
-	for i in range(0,orig_dims[0],40):
-		for j in range(0,orig_dims[1],40):
-			window = img[i:i+40, j:j+40].flatten()
+	for i in range(0,orig_dims[0],factor):
+		for j in range(0,orig_dims[1],factor):
+			window = img[i:i+factor, j:j+factor].flatten()
 			non_zero = np.delete(window, np.where(window==0))
 			if non_zero.size != 0:
-				downsized_img[i/40,j/40] = np.mean(non_zero)
+				downsized_img[i/factor,j/factor] = np.mean(non_zero)
 
 	return downsized_img
 
 ######################################################
 
-def get_data(input_type):
-	files = ['forensics', 'isat243', 'isat246', 'isat248', 'office', 'stairwell', 'spine'] 
+def get_data():
+#	files = ['forensics', 'isat243', 'isat246', 'isat248', 'office', 'stairwell', 'spine'] 
+#	files = ['test5','test6','test7','test8']
+	files = ['new2', 'new3', 'new4', 'new5', 'new6', 'new7', 'new8', 'new9', 'new10', 'new11', 'new12', 'new13', 'new14', 'new15', 'new16']
 	audio_list = []
 	depth_list = []
 	path = os.getcwd() 
 	for i in range(len(files)):
 		print "loading '%s' data..." %files[i]
-		with np.load(path+'/data_stereo/'+files[i]+'.npz') as d:
+		with np.load(path+'/data_stereo3/'+files[i]+'.npz') as d:
 			audio_list.append(d['audio'])
 			depth_list.append(d['depth'])
 	print "---------------------------------"
@@ -52,8 +56,8 @@ def get_data(input_type):
 			new_depth[counter] = downsize(d_map)
 			counter += 1
 
-	print "saving concatenated audio and downsized depth data saved as 'base_data.h5'...\n"
-	with h5py.File('base_data.h5', 'w') as hf:
+	print "saving concatenated audio and downsized depth data saved as 'base_data3.h5'...\n"
+	with h5py.File('base_data3.h5', 'w') as hf:
 		hf.create_dataset('audio', data=audio)
 		hf.create_dataset('depth', data=new_depth)
 
@@ -62,16 +66,16 @@ def get_data(input_type):
 def preprocess_data(input_type):
 	print "preprocessing data..."
 	path = os.getcwd()
-	with h5py.File(path+'/base_data.h5', 'r') as data:
+	with h5py.File(path+'/base_data3.h5', 'r') as data:
 		audio = data['audio'][:]	
 		depth = data['depth'][:] # shape: 13274, 12, 16
 
 	depth_reshaped = np.reshape(depth, (depth.shape[0],-1)) # shape: 13274, 192
 
 	if input_type == 1:
-		if os.path.isfile('input_spectrograms.h5'):
-			print "fetching spectrogram array from 'input_spectrograms.h5'..."
-			with h5py.File(path+'/input_spectrograms.h5', 'r') as sgrams:
+		if os.path.isfile('input_spectrograms3.h5'):
+			print "fetching spectrogram array from 'input_spectrograms3.h5'..."
+			with h5py.File(path+'/input_spectrograms3.h5', 'r') as sgrams:
 				input_set = sgrams['spectrograms'][:]
 		else: 
 			print "creating spectrogram set 0" 
@@ -90,8 +94,8 @@ def preprocess_data(input_type):
 				input_set[i,:,:,1] = spectro2
 			IS = input_set.shape
 			input_set = np.reshape(input_set, (IS[0],IS[1],IS[2],IS[3],1))
-			print "saving array of spectrograms as 'input_spectrograms.h5'..."
-			with h5py.File('input_spectrograms.h5', 'w') as sgrams:
+			print "saving array of spectrograms as 'input_spectrograms3.h5'..."
+			with h5py.File('input_spectrograms3.h5', 'w') as sgrams:
 				sgrams.create_dataset('spectrograms', data=input_set)
 	else: 
 		AS = audio.shape
@@ -103,9 +107,9 @@ def preprocess_data(input_type):
 
 def split_data(x, y, input_type):
 	if input_type == 1:
-		model_name = 'model_sets_spec.h5'
+		model_name = 'model_sets_spec3.h5'
 	else:
-		model_name = 'model_sets_rawA.h5'
+		model_name = 'model_sets_rawA3.h5'
 	
 	dims = list(x.shape)
 	dims[0] = -1
@@ -135,6 +139,17 @@ def split_data(x, y, input_type):
 		print "reshaping training vectors..."
 		xtrain = x[np.logical_not(xbool)].reshape(dims)
 		ytrain = y[np.logical_not(ybool)].reshape((-1,192))
+		print "x train before:", xtrain.shape
+		print "y train before:", ytrain.shape
+
+		print "shuffling for training set..."
+		combined = zip(xtrain, ytrain)
+		shuffle(combined)
+		xtrain, ytrain = zip(*combined)
+		xtrain = np.asarray(xtrain)
+		ytrain = np.asarray(ytrain)
+		print "x train after:", xtrain.shape
+		print "y train after:", ytrain.shape
 
 		print "saving data sets..."
 		with h5py.File(model_name, 'w') as sets:
@@ -152,8 +167,8 @@ def main():
 		print " "*6, "input_type - 1 for spectrograms, 2 for raw audio sample"
 		return
 
-	if not os.path.isfile('base_data.h5'):
-		get_data()
+#	if not os.path.isfile('base_data.h5'):
+#	get_data()
 	input_set, target = preprocess_data(int(argv[1]))
 	split_data(input_set, target, int(argv[1]))
 
