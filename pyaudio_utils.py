e6fb0351a30b750d459291cc1b55061d864439a2
Nathan Sprague <nathan.r.sprague@gmail.com>
2017-07-13 11:47:48 -0400

    another fix for ros_record.py

--- pyaudio_utils.py
+++ pyaudio_utils.py
@@ -15,31 +15,30 @@ class AudioPlayer(object):
         print self.wave_file.getnframes()
         self.pyaudio = pyaudio.PyAudio()
         self.out_stream = None
-
-    def play(self):
-        self.wave_file.rewind()
-        self.pyaudio.terminate()
-        
         self.pyaudio = pyaudio.PyAudio()
-        
-        if self.out_stream is not None:
-            self.out_stream.stop_stream()
-            self.out_stream.close()
 
+        
         self.out_stream = self.pyaudio.open(
             format=self.pyaudio.get_format_from_width(
                 self.wave_file.getsampwidth()),
             channels=self.wave_file.getnchannels(),
             rate=self.wave_file.getframerate(),
             output=True,
-            frames_per_buffer=min((self.wave_file.getnframes() +1,1024)),
+            frames_per_buffer=256,
             stream_callback=self._callback)
 
+        self.frame_bytes = (self.wave_file.getsampwidth() *
+                            self.wave_file.getnchannels())
+        self.playing = False
         self.out_stream.start_stream()
 
+    def play(self):
+        self.wave_file.rewind()
+        self.playing = True
+
     def is_playing(self):
-        return self.out_stream.is_active()
-    
+        return self.playing
+
     def shutdown(self):
         self.out_stream.stop_stream()
         self.out_stream.close()
@@ -47,7 +46,15 @@ class AudioPlayer(object):
 
     def _callback(self, in_data, frame_count, time_info, status):
         """ This is a pyAudio callback, not a ROS callback."""
-        data = self.wave_file.readframes(frame_count)
+        if self.playing is False:
+            data = '\x00' * self.wave_file.getsampwidth() * frame_count
+        else:
+            data = self.wave_file.readframes(frame_count)
+            frames_read = len(data) / self.frame_bytes
+            if frame_count != frames_read:
+                self.playing = False
+                data = data + '\x00' * (self.wave_file.getsampwidth() *
+                                        frame_count - frames_read)
         return (data, pyaudio.paContinue)
 
 
@@ -60,7 +67,7 @@ class AudioRecorder(object):
 
     def __init__(self, format=pyaudio.paInt16,
                  channels=1,
-                 rate=44100, frames_per_buffer=512):
+                 rate=44100, frames_per_buffer=256):
         self.pyaudio = pyaudio.PyAudio()
         self.channels = channels
         self.format = format
@@ -87,7 +94,7 @@ class AudioRecorder(object):
         self.done = False
         self._init_wav_file()
         self.target_callbacks = int(self.rate /
-                                    self.frames_per_buffer * duration)
+                                    self.frames_per_buffer * duration) + 2
         self.num_callbacks = 0
 
         if self.stream is not None:
@@ -125,9 +132,16 @@ class AudioRecorder(object):
         self.pyaudio.terminate()
 
     def callback(self, in_data, frame_count, time_info, status):
+        if self.num_callbacks > 1: # discard the first two (bad data)
+            self.wavefile.writeframes(in_data)
+            
+        if self.num_callbacks < self.target_callbacks:
+            done = pyaudio.paContinue
+        else:
+            done = pyaudio.paComplete
+
         self.num_callbacks += 1
-        self.wavefile.writeframes(in_data)
-        print "status", status
+        
         if self.num_callbacks < self.target_callbacks:
             return in_data, pyaudio.paContinue
         else:
@@ -138,8 +152,10 @@ class AudioRecorder(object):
 
 
 if __name__ == "__main__":
-    player = AudioPlayer('data/test.wav')
-    player.play()
+    player = AudioPlayer('data/16000to8000.02s.wav')
+    for i in range(10):
+        player.play()
+        time.sleep(.2)
 
     recorder = AudioRecorder()
     recorder.record(2)
