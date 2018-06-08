df1b16d30676357252646fa535849f029776d360
Nathan Sprague <nathan.r.sprague@gmail.com>
2018-06-08 14:14:30 -0400

    Trying out different network structures.

---
+++ network/network.py
@@ -0,0 +1,467 @@
+import os
+import os.path
+import argparse
+
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+
+import tensorflow as tf
+
+import keras
+import keras.layers as layers
+from keras import regularizers
+from keras.callbacks import ModelCheckpoint
+from keras.backend import floatx
+from keras.layers import Conv1D, Conv2D, Dense, MaxPooling2D,UpSampling2D, Input
+from keras.layers.core import Flatten, Reshape
+from keras.models import load_model, Sequential
+from keras.models import Model
+
+tf.logging.set_verbosity(tf.logging.WARN)
+tf.logging.set_verbosity(tf.logging.INFO)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+######################################################
+
+TARGET_WIDTH = 40
+TARGET_HEIGHT = 30
+TARGET_SIZE = TARGET_HEIGHT * TARGET_WIDTH
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data', help="preprocessed training and testing data")
+    parser.add_argument('model_folder',
+                        help="where to store models and results")
+    parser.add_argument('--epochs', type=int, default=200,
+                        help="how many epochs to train")
+    parser.add_argument('--lr', type=float, default=.001,
+                        help="initial learning rate")
+    parser.add_argument('--lr-reduce-every', type=int, default=50,
+                        help="how often to halve the learning rate")
+    parser.add_argument('--test', dest='test', action='store_true',
+                        help="test an existing model")
+    parser.set_defaults(test=False)
+
+    args = parser.parse_args()
+
+    if os.path.exists(args.model_folder):
+        print "output folder already exists."
+    else:
+        os.makedirs(args.model_folder)
+
+    model_file = args.model_folder + "/model.h5"
+    sets_file = args.data
+
+
+    from keras.backend.tensorflow_backend import set_session
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    set_session(tf.Session(config=config))
+
+    # Load testing data...
+    with h5py.File(sets_file, 'r') as sets:
+        x_test = sets['test_da'][:,0:2646,:]/32000.
+        y_test = np.log(1. + sets['test_depths'][:].reshape(-1, TARGET_SIZE))
+
+    if not args.test:
+        assert not os.path.isfile(model_file)
+        print "loading data..."
+        with h5py.File(sets_file, 'r') as sets:
+            x_train = sets['train_da'][:, 0:2646, :]/32000.
+            y_train = np.log(1. + sets['train_depths'][:].reshape(-1, TARGET_SIZE))
+
+            indices = np.random.permutation(x_train.shape[0])
+            np.take(x_train,indices,axis=0,out=x_train)
+            np.take(y_train,indices,axis=0,out=y_train)
+            
+        print "building model..."
+        model = build_and_train_model(x_train, y_train, args.model_folder,
+                                      args.lr, args.lr_reduce_every, args.epochs)
+
+    else:
+        print "loading model..."
+        model = load_model(model_file, custom_objects={'adjusted_mse':adjusted_mse})
+        plot_1d_convolutions(model)
+
+    model.summary()
+    run_model(model, x_test, y_test)
+
+    
+######################################################
+######################################################
+
+def plot_1d_convolutions(model):
+
+    print model.layers[0].get_weights()[1]
+    W = model.layers[0].get_weights()[0]
+    for i in range(80):
+        plt.subplot(16, 8, i+1)
+        plt.plot(W[:,0,i])
+    
+    plt.show()
+    print W.shape
+
+
+######################################################
+######################################################
+
+def raw_generator(x_train, y_train, batch_size=64, shift=.01,
+                  noise=.05, no_shift=False, shuffle=True, tone_noise=.25):
+    num_samples = x_train.shape[0]
+    sample_length = x_train.shape[1]
+    result_length = int(sample_length * (1 - shift))
+    batch_index = 0
+
+    while True:
+        # Shuffle before each epoch
+        if batch_index == 0 and shuffle:
+            indices = np.random.permutation(x_train.shape[0])
+            np.take(x_train,indices,axis=0,out=x_train)
+            np.take(y_train,indices,axis=0,out=y_train)
+
+        # Randomly crop the audio data...
+        if no_shift:
+            start_ind = np.zeros(batch_size, dtype='int32')
+        else:
+            start_ind = np.random.randint(sample_length - result_length + 1,
+                                          size=batch_size)
+        x_data = np.empty((batch_size, result_length * 2, 1))
+        for i in range(batch_size):
+            
+            # Concatenate the two channels...
+            x_data[i, 0:result_length, 0] = x_train[batch_index + i,
+                                                    start_ind[i]:start_ind[i] + result_length, 0]
+            x_data[i, result_length::, 0] = x_train[batch_index + i,
+                                                    start_ind[i]:start_ind[i] + result_length, 1]
+
+        # Add a sin wave with random freqency and phase...
+        if tone_noise > 0:
+            t = np.linspace(0, result_length /44100., result_length * 2 )
+            for i in range(batch_size):
+                pitch = np.random.random() * 115.0 + 20 # midi pitch
+                amp = np.random.random() * tone_noise
+                #people.sju.edu/~rhall/SoundingNumber/pitch_and_frequency.pdf 
+                freq = 440 * 2**((pitch - 69)/12.)
+                phase = np.pi * 2 * np.random.random()
+                tone = np.sin(2 * np.pi * freq * t + phase) * amp
+                x_data[i, :, 0] += tone
+
+            
+        # Random multiplier for all samples...
+        x_data *= (1. + np.random.randn(*x_data.shape) * noise)
+
+        y_data = y_train[batch_index:batch_index + batch_size, ...]
+
+        batch_index += batch_size
+
+        if batch_index > (num_samples - batch_size):
+            batch_index = 0
+
+        yield x_data, y_data
+
+
+######################################################
+######################################################
+
+def build_and_train_model(x_train, y_train, model_folder, lr,
+                          reduce_every, epochs):
+
+    L2 = 0#.000001
+    validation_split = .1
+    batch_size = 64
+
+    #Split validation data from the beginning of the training data...
+    end_val = int(validation_split * x_train.shape[0])
+    x_val = x_train[0:end_val, ...]
+    y_val = y_train[0:end_val, ...]
+
+    x_train = x_train[end_val::, ...]
+    y_train = y_train[end_val::, ...]
+
+    train_gen = raw_generator(x_train, y_train, batch_size=batch_size)
+
+    # Create validation set:
+    val_gen = raw_generator(x_val, y_val, batch_size=batch_size,
+                            noise=.00, no_shift=True, tone_noise=0)
+    #x_val, y_val = val_gen.next()
+
+    x_sample, _ = train_gen.next()
+    input_shape = x_sample.shape
+   
+    net = build_model_new(input_shape, L2)
+
+    adam = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999,
+                                 epsilon=1e-08, decay=0.0)
+    net.compile(optimizer=adam, loss=adjusted_mse)
+    net.summary()
+
+    # Configure callbacks:
+    filepath = model_folder + '/model.{epoch:02d}.h5'
+    checkpoint = ModelCheckpoint(filepath, monitor='loss',
+                                 verbose=0,
+                                 save_best_only=False, save_weights_only=False,
+                                 mode='auto', period=25)
+    lr_reducer = LRReducer(reduce_every)
+
+    tensorboard = keras.callbacks.TensorBoard(log_dir=model_folder,
+                                              histogram_freq=0,
+                                              write_graph=True,
+                                              write_images=False)
+
+    csv_logger = keras.callbacks.CSVLogger(model_folder+'/log.csv')
+
+    callbacks_list = [checkpoint, lr_reducer, csv_logger, tensorboard]
+
+    # Perform the training:
+    hist = net.fit_generator(train_gen,
+                             steps_per_epoch=x_train.shape[0]//batch_size,
+                             epochs=epochs, callbacks=callbacks_list,
+                             validation_data=val_gen,
+                             validation_steps=end_val//batch_size)
+
+    net.save(model_folder+'/model.h5')
+    return net
+
+
+def build_model_new(input_shape, L2):
+    inputs = Input(shape=input_shape[1::])
+    conv = Conv1D(100, (256),
+               strides=(2), use_bias=True,
+               activation='elu',
+               input_shape=input_shape[1::],
+               kernel_regularizer=regularizers.l2(L2))
+    x = conv(inputs)
+    #x = layers.BatchNormalization(momentum=.99)(x)
+    conv_output_size = conv.output_shape[1]
+    x = Reshape((conv_output_size, 100, 1))(x)
+    x = MaxPooling2D(pool_size=(8, 1), strides=None,
+                     padding='valid')(x)
+
+    
+    
+    x = Conv2D(32, (3, 3), strides=(2, 2), use_bias=True,
+               activation='elu',padding='same',
+               kernel_regularizer=regularizers.l2(L2))(x)
+    #x = layers.BatchNormalization(momentum=.99)(x)
+    x = Conv2D(128, (3, 3), strides=(2, 2), use_bias=True,
+               activation='elu',padding='same',
+               kernel_regularizer=regularizers.l2(L2))(x)
+    #x = layers.BatchNormalization(momentum=.99)(x)
+
+    for i in range(4):
+        residual = x
+
+        for j in range(3):
+            x = layers.Activation('elu')(x)
+            x = layers.SeparableConv2D(128, (3, 3),
+                                       padding='same',
+                                       use_bias=True)(x)
+            #x = layers.BatchNormalization(momentum=.99)(x)
+        x = layers.add([x, residual])
+
+    x = layers.Activation('elu')(x)
+    x = layers.SeparableConv2D(16, (3, 3),
+                               padding='same',
+                               use_bias=True)(x)
+    #x = layers.BatchNormalization(momentum=.99)(x)
+    x = Flatten()(x)
+  
+   
+    x = Dense(600, activation='elu',use_bias=True,
+              kernel_regularizer=regularizers.l2(L2))(x)
+    #x = layers.BatchNormalization(momentum=.99)(x)
+    x = Dense(600, activation='elu',use_bias=True,
+              kernel_regularizer=regularizers.l2(L2))(x)
+    #x = layers.BatchNormalization(momentum=.99)(x)
+    net = Dense(1200, activation='linear',
+                  kernel_regularizer=regularizers.l2(L2))(x)
+
+    # net.add(Reshape((40, 30, 1)))
+    # net.add(Conv2D(32, (3,3), strides=(1,1), activation='relu',padding='same',
+    #                kernel_regularizer=regularizers.l2(L2)))
+    # net.add(Conv2D(32, (3,3), strides=(1,1), activation='relu',padding='same',
+    #                kernel_regularizer=regularizers.l2(L2)))
+    # net.add(Conv2D(1, (3,3), strides=(1,1), activation='linear',padding='same',
+    #                kernel_regularizer=regularizers.l2(L2)))
+
+    # net.add(Flatten())
+    net = Model(inputs=inputs, outputs=net)
+    return net
+
+
+
+def build_model(input_shape, L2):
+    net = Sequential()
+    
+    # net.add(Conv1D(120, (512),
+    #                strides=(1),
+    #                activation='relu',
+    #                input_shape=input_shape[1::],
+    #                kernel_regularizer=regularizers.l2(L2)))
+    # conv_output_size = net.layers[0].compute_output_shape(input_shape)[1]
+    # net.add(Reshape((conv_output_size,120,1)))
+    # net.add(MaxPooling2D(pool_size=(16, 1), strides=None,
+    #                      padding='valid'))
+    # net.add(Conv2D(64, (5,5), strides=(2,5), activation='relu',
+    #                kernel_regularizer=regularizers.l2(L2)))
+    # net.add(Conv2D(64, (5,5), strides=(2,1), activation='relu',
+    #                kernel_regularizer=regularizers.l2(L2)))
+    # net.add(Conv2D(32, (3,3), strides=(1,1), activation='relu',
+    #                kernel_regularizer=regularizers.l2(L2)))
+    # net.add(Flatten())
+    
+    net.add(Conv1D(100, (512),
+                   strides=(1),
+                   activation='relu',
+                   input_shape=input_shape[1::],
+                   kernel_regularizer=regularizers.l2(L2)))
+    conv_output_size = net.layers[0].compute_output_shape(input_shape)[1]
+    net.add(Reshape((conv_output_size, 100, 1)))
+    net.add(MaxPooling2D(pool_size=(16, 1), strides=None,
+                         padding='valid'))
+    net.add(Conv2D(32, (3, 3), strides=(2, 2), activation='relu',padding='same',
+                   kernel_regularizer=regularizers.l2(L2)))
+    net.add(Conv2D(32, (3, 3), strides=(2, 2), activation='relu',padding='same',
+                   kernel_regularizer=regularizers.l2(L2)))
+    #net.add(Conv2D(32, (3, 3), strides=(1, 1), activation='relu',padding='same',
+    #               kernel_regularizer=regularizers.l2(L2)))
+    net.add(Conv2D(16, (1, 1), strides=(1, 1), activation='relu',padding='same',
+                   kernel_regularizer=regularizers.l2(L2)))
+    
+    net.add(Flatten())
+    net.add(Dense(600, activation='relu',
+                  kernel_regularizer=regularizers.l2(L2)))
+    net.add(Dense(600, activation='relu',
+                  kernel_regularizer=regularizers.l2(L2)))
+    net.add(Dense(1200, activation='relu',
+                  kernel_regularizer=regularizers.l2(L2)))
+
+    net.add(Reshape((40, 30, 1)))
+    net.add(Conv2D(32, (3,3), strides=(1,1), activation='relu',padding='same',
+                   kernel_regularizer=regularizers.l2(L2)))
+    net.add(Conv2D(32, (3,3), strides=(1,1), activation='relu',padding='same',
+                   kernel_regularizer=regularizers.l2(L2)))
+    net.add(Conv2D(1, (3,3), strides=(1,1), activation='linear',padding='same',
+                   kernel_regularizer=regularizers.l2(L2)))
+
+    net.add(Flatten())
+    return net
+
+
+
+######################################################
+
+class LRReducer(keras.callbacks.LearningRateScheduler):
+
+    def __init__(self, reduce_every, reduce_by=.5, verbose=0):
+        super(LRReducer, self).__init__(self.schedule, verbose)
+        self.reduce_every = reduce_every
+        self.reduce_by = reduce_by
+        
+
+    def schedule(self, epoch, lr):
+        if epoch != 0 and (epoch % self.reduce_every) == 0:
+            return lr * self.reduce_by
+        else:
+            return lr
+
+
+######################################################
+
+def run_model(net, x_test, y_test):
+    gen = raw_generator(x_test, y_test, noise=0, no_shift=True,
+                        batch_size=x_test.shape[0], shuffle=False)
+    x_test = next(gen)[0]
+    predictions = net.predict(x_test, batch_size=64)
+    loss = net.evaluate(x_test, y_test)
+    print "\nTEST LOSS:", loss
+    view_average_error(np.exp(y_test)-1,np.exp(predictions)-1)
+    for i in range(100, 2000, 110):
+        view_depth_maps(i, x_test, np.exp(y_test)-1, np.exp(predictions)-1)
+
+#####################################################
+
+def adjusted_mse(y_true, y_pred):
+    zero = tf.constant(0, dtype=floatx())
+    ok_entries = tf.not_equal(y_true, zero)
+    safe_targets = tf.where(ok_entries, y_true, y_pred)
+    sqr = tf.square(y_pred - safe_targets)
+    valid = tf.cast(ok_entries, floatx())
+    num_ok = tf.reduce_sum(valid, axis=-1) # count OK entries
+    num_ok = tf.maximum(num_ok, tf.ones_like(num_ok)) # avoid divide by zero
+    return tf.reduce_sum(sqr, axis=-1) / num_ok
+
+#####################################################
+
+def view_average_error(ytrue, ypred):
+    error = np.reshape(ypred-ytrue, (-1,12,16))
+    avg_error = np.mean(error, axis=0)
+    stdev = np.std(avg_error)
+    avg_val = np.mean(avg_error)
+    rng = (avg_val-(3*stdev),avg_val+(3*stdev))
+    error_map = plt.imshow(avg_error, clim=rng, cmap="Greys", interpolation='none')
+    plt.title("Absolute Average Error")
+    plt.show()
+
+#####################################################
+
+def view_depth_maps(index, xtest, ytrue, ypred):
+    all_error = ypred-ytrue
+    avg_error = np.mean(all_error)
+    stdev = np.std(all_error)
+    rng = (avg_error-(3*stdev),avg_error+(3*stdev))
+    for i in range(0, ytrue.shape[0], 50):
+        print 
+        for j in range(10):
+            index = -1
+
+            index = np.random.randint(ytrue.shape[0])
+#            while index == -1:
+#                index = np.random.randint(ytrue.shape[0])
+#                if np.sum(ytrue[index] > 7000) + np.sum(ytrue[index] ==0) < 20:
+#                    true = np.reshape(ytrue[index], (12,16))
+#                    pred = np.reshape(ypred[index], (12,16))
+#                else:
+#                    index = -1
+
+            print index  
+            true = np.reshape(ytrue[index], (TARGET_HEIGHT,TARGET_WIDTH))
+            pred = np.reshape(ypred[index], (TARGET_HEIGHT,TARGET_WIDTH))
+            #true = np.log( 1 + np.reshape(ytrue[index], (12,16)))
+            #pred = np.log(1 + np.reshape(ypred[index], (12,16)))
+            error = pred - true
+
+
+            #min_depth = np.min(true[true != 0])
+            #max_depth = np.max(true)
+            #min_depth = np.log(300)
+            #max_depth = np.log(10000)
+            min_depth = 300
+            max_depth = 7000#1800
+
+            ax0 = plt.subplot(10,4,j*4 + 1)
+            audio_plot = plt.plot(xtest[index,...])
+            ax0.set_title("Audio")
+
+            ax1 = plt.subplot(10,4,j*4 + 2)
+            true_map = plt.imshow(true, clim=(min_depth, max_depth),
+                                  interpolation='none')
+            ax1.set_title("True Depth")
+
+            ax2 = plt.subplot(10,4,j*4 + 3)
+            pred_map = plt.imshow(pred, clim=(min_depth, max_depth),
+                                  interpolation='none')
+            ax2.set_title("Predicted Depth")
+
+            ax3 = plt.subplot(10,4,j*4 + 4)
+            error_map = plt.imshow(error, clim=rng, cmap="Greys",
+                                   interpolation='none')
+            ax3.set_title("Squared Error Map")
+        plt.show()
+
+#####################################################
+
+if __name__ == "__main__":
+    main()
